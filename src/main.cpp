762589a545055bd63675dc7672df31780fe215c8
lixx299792458 <15726089100@qq.com>
2025-02-04 22:38:43 +0800

    先测试蓝牙心率带的连接

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,49 @@
-#include <Arduino.h>
-
-// put function declarations here:
-int myFunction(int, int);
-
+#include "NimBLEDevice.h"
+ 
 void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+	Serial.begin(115200, SERIAL_8N1);
+	Serial.printf("running");
+
+    NimBLEDevice::init("");
+    NimBLEScan *pScan = NimBLEDevice::getScan();
+    NimBLEScanResults results = pScan->getResults(10 * 1000);
+    NimBLEUUID service_HR_UUID(BLEUUID((uint16_t)0x180D));
+	NimBLEUUID char_HR_UUID(BLEUUID((uint16_t)0x2A37));
+
+    for (int i = 0; i < results.getCount(); i++) {
+        const NimBLEAdvertisedDevice *device = results.getDevice(i);
+        if (device->isAdvertisingService(service_HR_UUID)) {
+            NimBLEClient *pClient = NimBLEDevice::createClient();
+			Serial.printf("founded");
+            if (!pClient) { // Make sure the client was created
+				
+                break;
+            }
+ 
+            if (pClient->connect(device)) {
+                NimBLERemoteService *pService = pClient->getService(service_HR_UUID);
+				
+
+                if (pService != nullptr) {
+                    NimBLERemoteCharacteristic *pCharacteristic = pService->getCharacteristic(char_HR_UUID);
+ 
+                    if (pCharacteristic != nullptr) {
+						Serial.printf("data ready");
+						Serial.println((pCharacteristic->readValue()).size());
+                        // print or do whatever you need with the value
+                    }
+                }
+            } else {
+                // failed to connect
+				Serial.printf("failed to connect");
+            }
+ 
+            NimBLEDevice::deleteClient(pClient);
+        }
+    }
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
-}
-
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+void loop()
+{
+  
 }
